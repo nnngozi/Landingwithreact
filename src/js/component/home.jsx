e56ac79e9a857a781373c365704eb431e406759f
nnngozi <101212459+nnngozi@users.noreply.github.com>
2022-05-23 19:33:28 -0400

    Initial commit

---
+++ src/js/component/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./tools/Navbar.jsx";
+
+//create your first component
+const Home = () => {
+	return (
+		<div>
+			<Navbar />
+		</div>
+	);
+};
+
+export default Home;
